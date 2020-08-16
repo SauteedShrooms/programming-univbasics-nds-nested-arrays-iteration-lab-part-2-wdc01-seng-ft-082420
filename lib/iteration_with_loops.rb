71472eb13c0398063961bc03636e66e7e33dbfba
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-16 21:58:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,6 @@ results = []
 row_index = 0 
 while row_index < src.count do
   element_index = 0 
-  smallest_number = 0 
   while element_index < src[row_index].count do
     if src[row_index][element_index] < src[row_index][element_index]
       results << src[row_index][element_index]
